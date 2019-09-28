2bc32c7973a4ad57a5ad4584141a6e71b4d93deb
YuraT <ytatisch@gmail.com>
2019-09-28 08:39:15 -0700

    completed exercise 00

---
+++ 00_daenerys.py
@@ -0,0 +1,9 @@
+def main():
+    name = input("Who goes there?")
+    if name == "DHTFHNUQARFMQMKGSPRLRSKBCMD":
+        print("Welcome, Daenerys.")
+    elif name == "Dany":
+        print("Dany who?")
+    else:
+        print("Move along, now.")
+main()
