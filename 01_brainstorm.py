cea13e58794bb23b49d65eccf611d6e1b8de17e3
YuraT <ytatisch@gmail.com>
2019-09-27 17:43:49 -0700

    completed exercise 1

--- 01_brainstorm.py
+++ 01_brainstorm.py
@@ -1,1 +1,15 @@
-list_categories = ["metal objects", "wooden objects", "dangerous objects", "silver objects", "bright objects", "boring objects"]
+import random
+
+def main():
+    categories_list = ["red objects", "green objects", "blue objects"]
+    category = random.randint(0, 2)
+    
+    answers = []
+    for i in range(10):
+        temp = input(categories_list[category] + ": ")
+        answers.append(temp)
+
+    for i in range(10):
+        print("{:^24s}".format(answers[i]))
+
+main()
